3f7a65fd83b8cc89c93100aefbbd2b113db7472a
mostafasoliman474 <as6615150@gmail.com>
2023-12-14 00:12:56 +0200

    env

--- shopbackend/routes/stripe.js
+++ shopbackend/routes/stripe.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const key=process.env.STRIPE_KEY;
-const stripe = require("stripe")('sk_test_51OIBXGGTHVRNZlBtI28Ps0UsUh6U1YBzvWodflPUzGrMXpSsG5SQjI6PfJqBFJAaRl902qmTjUFqyfxZsNMySMSV00Whd9fWfp');
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 router.post("/payment", (req, res) => {
   stripe.charges.create(
